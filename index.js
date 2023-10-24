cbedd98fd556c3bc91c8263c29bc71c9b11c1a78
Abdoulfatah Kartish <abdoulfatah.kartish@msg.group>
2023-10-24 12:48:32 +0200

    port changed

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 9000;
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
